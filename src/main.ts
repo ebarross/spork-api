2e224e7623041f5522c4b56e774b6ad9436ae169
Emanoel Barros <ebarros297@gmail.com>
2022-03-24 19:12:31 -0300

    chore: add fastify adapter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 
 const defaultPort = 3000;
+const applicationPort = process.env.APP_LOCAL_PORT || defaultPort;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.APP_LOCAL_PORT || defaultPort);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+  await app.listen(applicationPort);
 }
 bootstrap();
