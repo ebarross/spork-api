9ca5100ae6215fbe62babeec798ef340550395d9
Emanoel Barros <ebarros297@gmail.com>
2022-03-25 12:05:57 -0300

    feat: enable jwt guard for all routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
+import { APP_GUARD } from '@nestjs/core';
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Module({
   imports: [UserModule],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
