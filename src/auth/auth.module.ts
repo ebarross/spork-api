6c812e0a0b8b3898f3eaea74207955bfeda769fa
Emanoel Barros <ebarros297@gmail.com>
2022-03-25 11:52:42 -0300

    feat: register jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UserModule } from '../user/user.module';
 import { AuthService } from './auth.service';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 
+const { secret, expiresIn } = jwtConstants;
+
 @Module({
-  imports: [UserModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret,
+      signOptions: { expiresIn },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,3 +1,4 @@
 export const jwtConstants = {
   secret: process.env.APP_JWT_SECRET,
+  expiresIn: `${process.env.APP_JWT_EXPIRATION_HOURS}h`,
 };
