e8b79c2e94af073545298afb83f3f126529e5fb6
Emanoel Barros <ebarros297@gmail.com>
2022-03-25 15:40:14 -0300

    feat: add role decorator

---
+++ src/role/role.decorator.ts
@@ -0,0 +1,5 @@
+import { SetMetadata } from '@nestjs/common';
+import { RoleEnum } from './role.enum';
+
+export const ROLE_KEY = 'role';
+export const Role = (role: RoleEnum) => SetMetadata(ROLE_KEY, role);
